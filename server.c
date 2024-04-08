8c9d5d42747fd3d40f0b123d5de1bae452e6344e
박성우 <tjddn0513@gmail.com>
2024-04-08 09:54:24 +0900

    pipe를 사용하여 스레드간 통신하도록 변경

--- server.c
+++ server.c
@@ -2,18 +2,28 @@
 #include <pthread.h>
 
 #define SERVERPORT 9000
+#define BUFSIZE 10000
 
 void _bind(SOCKET sock, int port);
 void _listen(SOCKET sock, int maxconn);
 SOCKET _accept(SOCKET sock, struct sockaddr_in *client_addr);
+void _print_connected_client(struct sockaddr_in* client_addr);
+void _print_disconnected_client(struct sockaddr_in* client_addr);
 
 int main(int argc, char const *argv[])
 {
     int retval;
-
+    char buf[BUFSIZE];
     SOCKET listen_sock = create_socket();
     _bind(listen_sock, SERVERPORT);
     _listen(listen_sock, SOMAXCONN);
+
+    int pipefd[2];
+    if (pipe(pipefd) < 0)
+    {
+        perror("pipe");
+        return 0;
+    }
 
     SOCKET client_sock;
     struct sockaddr_in client_addr;
@@ -22,11 +32,43 @@ int main(int argc, char const *argv[])
     while (1)
     {
         client_sock = _accept(listen_sock, &client_addr);
-        retval = pthread_create(&wthr, NULL, process_client, (void *)(long long)client_sock);
-        if (retval != 0)
-        {
-            close(client_sock);
+        _print_connected_client(&client_addr);
+        
+        while(1) {
+            retval = recv(client_sock, buf, BUFSIZE, 0);
+            if (retval == SOCKET_ERROR)
+            {
+                err_display("recv()");
+                break;
+            }
+
+            else if (retval == 0)
+            {
+                break;
+            }
+
+            Job job = {client_sock, buf, pipefd[1]};
+            
+            retval = pthread_create(&wthr, NULL, process_client, (void *) &job);
+            if (retval != 0)
+            {
+                close(client_sock);
+            }
+            printf("대기중!");
+            pthread_join(wthr, NULL);
+            printf("종료!");
+            
+            read(pipefd[0], buf, sizeof(buf));
+
+            retval = send(client_sock, buf, BUFSIZE, 0);
+            if (retval == SOCKET_ERROR)
+            {
+                err_display("send()");
+                break;
+            }
         }
+        close(client_sock);
+        _print_disconnected_client(&client_addr);
     }
 
     close(listen_sock);
@@ -73,4 +115,16 @@ SOCKET _accept(SOCKET sock, struct sockaddr_in *client_addr)
     }
 
     return client_sock;
+}
+
+void _print_connected_client(struct sockaddr_in* client_addr) {
+    char client_ip[20];
+    inet_ntop(AF_INET, &client_addr->sin_addr, client_ip, sizeof(client_ip));
+    printf("[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n", client_ip, ntohs(client_addr->sin_port));
+}
+
+void _print_disconnected_client(struct sockaddr_in* client_addr) {
+    char client_ip[20];
+    inet_ntop(AF_INET, &client_addr->sin_addr, client_ip, sizeof(client_ip));
+    printf("[TCP 서버] 클라이언트 접속 종료: IP 주소=%s, 포트 번호=%d\n", client_ip, ntohs(client_addr->sin_port));
 }
--- wthr.h
+++ wthr.h
@@ -4,143 +4,129 @@
 #include <dirent.h>
 #include <time.h>
 
-#define BUFSIZE 10000
 #define LOGNAME "FILE_SERVER"
+#define BUFSIZE 10000
 
 HashMap *_parse_body(int hash_size, char *buf);
 
+typedef struct Job {
+    SOCKET client_sock;
+    char *data;
+    int pipefd;
+} Job;
+
 void *process_client(void *arg)
-{
+{   
     int retval;
-    char buf[BUFSIZE + 1];
     int hash_size = 10;
     FILE *fp;
-    SOCKET client_sock = (SOCKET)(long long)arg;
-    while (1)
+    Job *job = (Job *) arg;
+    char response[BUFSIZE];
+
+    HashMap *hashMap = _parse_body(hash_size, job->data);
+
+    char *option = get(hashMap, OPTION);
+    if (*option == WRITE)
     {
-        retval = recv(client_sock, buf, BUFSIZE, 0);
-        if (retval == SOCKET_ERROR)
+        char *filename = get(hashMap, FILENAME);
+        int offset = atoi(get(hashMap, OFFSET));
+        char *data = get(hashMap, DATA);
+
+        fp = fopen(filename, "rb+");
+        if (fp == NULL)
         {
-            err_display("recv()");
-            break;
-        }
-
-        else if (retval == 0)
-        {
-            break;
-        }
-
-        buf[retval] = '\0';
-        HashMap *hashMap = _parse_body(hash_size, buf);
-
-        char *option = get(hashMap, OPTION);
-        if (*option == WRITE)
-        {
-            char *filename = get(hashMap, FILENAME);
-            int offset = atoi(get(hashMap, OFFSET));
-            char *data = get(hashMap, DATA);
-
-            fp = fopen(filename, "rb+");
+            fp = fopen(filename, "wb");
             if (fp == NULL)
             {
-                fp = fopen(filename, "wb");
-                if (fp == NULL)
-                {
-                    perror("Error opening file");
-                }
-                offset = 0;
+                perror("Error opening file");
             }
+            offset = 0;
+        }
 
+        fseek(fp, 0, SEEK_END);
+        long fileSize = ftell(fp);
+        if (offset > fileSize)
+        {
+            offset = fileSize;
+        }
+
+        fseek(fp, offset, SEEK_SET);
+        fwrite(data, sizeof(char), strlen(data), fp);
+
+        char logMessage[BUFSIZE];
+        snprintf(logMessage, BUFSIZE, "option=%c, filename=%s, offset=%d, data=%s", WRITE, filename, offset, data);
+        write_log(LOG_INFO, logMessage);
+
+        fclose(fp);
+        strcpy(response, "Data written successfully");
+    }
+
+    else if (*option == READ)
+    {
+        char *filename = get(hashMap, FILENAME);
+        int offset = atoi(get(hashMap, OFFSET));
+        int length = atoi(get(hashMap, LENGTH));
+
+        fp = fopen(filename, "r");
+        if (fp == NULL) {
+            strcpy(response, "Error opening file");
+        }
+        else {
             fseek(fp, 0, SEEK_END);
             long fileSize = ftell(fp);
             if (offset > fileSize)
             {
-                offset = fileSize;
+                strcpy(response, "Offset is greater than file size");
             }
-
-            fseek(fp, offset, SEEK_SET);
-            fwrite(data, sizeof(char), strlen(data), fp);
-
-            char logMessage[BUFSIZE];
-            snprintf(logMessage, BUFSIZE, "option=%c, filename=%s, offset=%d, data=%s", WRITE, filename, offset, data);
-            write_log(LOG_INFO, logMessage);
-
+            else
+            {
+                fseek(fp, offset, SEEK_SET);
+                length = (offset + length > fileSize) ? fileSize - offset : length;
+                fread(response, sizeof(char), length, fp);
+                response[length] = '\0';
+            }
             fclose(fp);
-            strcpy(buf, "Data written successfully");
         }
-
-        else if (*option == READ)
-        {
-            char *filename = get(hashMap, FILENAME);
-            int offset = atoi(get(hashMap, OFFSET));
-            int length = atoi(get(hashMap, LENGTH));
-
-            fp = fopen(filename, "r");
-            if (fp == NULL) {
-                strcpy(buf, "Error opening file");
-            }
-            else {
-                fseek(fp, 0, SEEK_END);
-                long fileSize = ftell(fp);
-                if (offset > fileSize)
-                {
-                    strcpy(buf, "Offset is greater than file size");
-                }
-                else
-                {
-                    fseek(fp, offset, SEEK_SET);
-                    length = (offset + length > fileSize) ? fileSize - offset : length;
-                    fread(buf, sizeof(char), length, fp);
-                    buf[length] = '\0';
-                }
-                fclose(fp);
-            }
-        }
-
-        else if (*option == DELETE)
-        {
-            char *filename = get(hashMap, FILENAME);
-            if (unlink(filename) == 0) {
-                char logMessage[BUFSIZE];
-                snprintf(logMessage, BUFSIZE, "option=%c, filename=%s", DELETE, filename);
-                write_log(LOG_INFO, logMessage);
-                strcpy(buf, "File deleted successfully");
-            } 
-            else {
-                strcpy(buf, "Error deleting file");
-            }
-        }
-        else if (*option == LIST)
-        {
-            DIR *dir;
-            struct dirent *entry;
-
-            dir = opendir(".");
-            if (dir == NULL)
-            {
-                perror("Error opening dir");
-            }
-
-            while ((entry = readdir(dir)) != NULL)
-            {
-                strcat(buf, entry->d_name);
-                strcat(buf, "\n");
-            }
-
-            closedir(dir);
-        }
-
-        retval = send(client_sock, buf, BUFSIZE, 0);
-        if (retval == SOCKET_ERROR)
-        {
-            err_display("send()");
-            break;
-        }
-
-        freeHashMap(hashMap);
     }
-    close(client_sock);
-    return 0;
+
+    else if (*option == DELETE)
+    {
+        char *filename = get(hashMap, FILENAME);
+        if (unlink(filename) == 0) {
+            char logMessage[BUFSIZE];
+            snprintf(logMessage, BUFSIZE, "option=%c, filename=%s", DELETE, filename);
+            write_log(LOG_INFO, logMessage);
+            strcpy(response, "File deleted successfully");
+        } 
+        else {
+            strcpy(response, "Error deleting file");
+        }
+    }
+
+    else if (*option == LIST)
+    {
+        DIR *dir;
+        struct dirent *entry;
+
+        dir = opendir(".");
+        if (dir == NULL)
+        {
+            perror("Error opening dir");
+        }
+
+        while ((entry = readdir(dir)) != NULL)
+        {
+            strcat(response, entry->d_name);
+            strcat(response, "\n");
+        }
+
+        closedir(dir);
+    }
+
+    freeHashMap(hashMap);
+
+    write(job->pipefd, response, strlen(response));
+    return NULL;
 };
 
 HashMap *_parse_body(int hash_size, char *buf)
